b16dbaa281f22151baf8c77024cd9f5165e0d43e
pranay9949 <139853595+pranay9949@users.noreply.github.com>
2023-10-02 13:12:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your code here
+let images=document.getElementsByClassName("image");
+let container=document.getElementById("parent")
+function imageEnter(event){
+    event.dataTransfer.setData("srcId",event.target.id);
+    
+}
+function imageOver(event){
+    event.preventDefault();
+    
+}
+function imageDropped(event){
+
+    let srcIde=event.dataTransfer.getData("srcId");
+    let sourceElement=document.getElementById(srcIde);
+    targetElemnt=event.target;
+    let firstElement=sourceElement.nextElementSibling;
+    let secondElement=targetElemnt.nextElementSibling;
+    container.insertBefore(targetElemnt,firstElement);
+    container.insertBefore(sourceElement,secondElement);
+
+}
+
+for(let i=0;i<images.length;i++){
+	images[i].addEventListener("dragstart",imageEnter);
+    images[i].addEventListener("dragover",imageOver);
+    images[i].addEventListener("drop",imageDropped);
+}
